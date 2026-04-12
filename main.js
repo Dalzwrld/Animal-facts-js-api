036a9c176f3710e310bda96b0b48b522622b1d11
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-12 17:01:57 +0300

    feat: add main js file

--- main.js
+++ main.js
@@ -3,7 +3,20 @@ const animal = "";
 
 let fauna = JSON.parse(localStorage.getItem("animal")) || [];
 
+async function displayAnimal() {
+    const endpoint = `https://api.api-ninjas.com/v1/animals?name=`;
 
+    try {
+        const response = await fetch(endpoint);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+            
+        }
+    } catch (error) {
+        
+    }
+}
 
 
 const name = animal.name;
