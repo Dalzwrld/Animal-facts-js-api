fecfc420df4d8fc3b09f48f1d12b1d8fe21844a0
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-12 17:17:13 +0300

    feat: add main js file

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-const API_key = "O6gDOjX0GLmJOBMyTWDoj5RMeMw3Y0LEovT0oWvF";
-const animal = "";
-
 let fauna = JSON.parse(localStorage.getItem("animal")) || [];
 
+const API_key = "O6gDOjX0GLmJOBMyTWDoj5RMeMw3Y0LEovT0oWvF";
+
+const animals = ["lion", "cat", "dog", "tiger", "elephant",];
+
 async function displayAnimal() {
-    const endpoint = `https://api.api-ninjas.com/v1/animals?name=`;
+    const endpoint = `https://api.api-ninjas.com/v1/animals?name={randomAnimal}`;
 
     try {
-        const response = await fetch(endpoint);
+        const response = await fetch(endpoint, {
+            method: "GET",
+        });
 
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
-            
         }
+
+        const data = await response.json();
+
+        const animal = data[0];
+
+        const name = animal.name;
+        const taxonomy = animal?.taxonomy || {};
+        const locations = animal?.locations || [];
+        const characteristics = animal?.characteristics || {};
+
+        const habitat = animal.characteristics?.habitat;
+        const diet = animal.characteristics?.diet;
+        const group = animal.characteristics?.group;
+        const lifespan = animal.characteristics?.lifespan;
     } catch (error) {
-        
+        console.error("Error fetching your animal:", error);
+        document.querySelector().textContent = "Unable to retrieve animal right now."
     }
 }
-
-
-const name = animal.name;
-const taxonomy = animal?.taxonomy || {};
-const locations = animal?.locations || [];
-const characteristics = animal?.characteristics || {};
-
-const habitat = animal.characteristics?.habitat;
-const diet = animal.characteristics?.diet;
-const group = animal.characteristics?.group;
-const lifeSpan = animal.characteristics?.lifespan;
-
-const animals = ["lion", "cat", "dog", "tiger", "elephant",]
