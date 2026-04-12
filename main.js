b0db141efa99b8937914e6c1aab3ad565b5b880f
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-12 15:24:27 +0300

    feat: add main js file

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ let fauna = JSON.parse(localStorage.getItem("animal")) || [];
 const animal = data[0];
 
 const name = animal.name;
-const taxonomy = animal?.taxonomy || [];
+const taxonomy = animal?.taxonomy || {};
 const locations = animal?.locations || [];
-const characteristics = animal?.characteristics || [];
+const characteristics = animal?.characteristics || {};
 
-const habitat = animal.characteristics.habitat;
-const diet = animal.characteristics.diet;
-const group = animal.characteristics.group;
-const lifeSpan = animal.characteristics.lifespan;
+const habitat = animal.characteristics?.habitat;
+const diet = animal.characteristics?.diet;
+const group = animal.characteristics?.group;
+const lifeSpan = animal.characteristics?.lifespan;
