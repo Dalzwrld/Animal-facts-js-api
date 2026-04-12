a75b9a0092cbc012858029571e9ff0d6a08abeac
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-12 17:31:24 +0300

    feat: add main js file

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const API_key = "O6gDOjX0GLmJOBMyTWDoj5RMeMw3Y0LEovT0oWvF";
 const animals = ["lion", "cat", "dog", "tiger", "elephant",];
 
 async function displayAnimal() {
+    const randomAnimal = animals[Math.floor(Math.random() * animals.length)]
+
     const endpoint = `https://api.api-ninjas.com/v1/animals?name={randomAnimal}`;
 
     try {
@@ -19,18 +21,25 @@ async function displayAnimal() {
         const data = await response.json();
 
         const animal = data[0];
-
-        const name = animal.name;
-        const taxonomy = animal?.taxonomy || {};
-        const locations = animal?.locations || [];
-        const characteristics = animal?.characteristics || {};
-
-        const habitat = animal.characteristics?.habitat;
-        const diet = animal.characteristics?.diet;
-        const group = animal.characteristics?.group;
-        const lifespan = animal.characteristics?.lifespan;
     } catch (error) {
         console.error("Error fetching your animal:", error);
         document.querySelector().textContent = "Unable to retrieve animal right now."
+    }
+}
+
+const name = animal.name || " ";
+const taxonomy = animal.taxonomy || {};
+const locations = animal.locations || [];
+const characteristics = animal.characteristics || {};
+
+const habitat = animal.characteristics.habitat || " ";
+const diet = animal.characteristics.diet || " ";
+const group = animal.characteristics.group || " ";
+const lifespan = animal.characteristics.lifespan || " ";
+
+let taxonomyItems = "";
+for (let key in taxonomy) {
+    function normalCase() {
+        const item = item.toLowerCase("").split;
     }
 }
