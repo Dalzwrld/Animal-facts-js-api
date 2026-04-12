dab15fb7dc5f7e1baf231c1a63fa7e2eafa782ec
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-12 15:19:00 +0300

    feat: add main js file

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const animal = data[0];
 
 const name = animal.name;
 const taxonomy = animal?.taxonomy || [];
+const locations = animal?.locations || [];
+const characteristics = animal?.characteristics || [];
+
+const habitat = animal.characteristics.habitat;
+const diet = animal.characteristics.diet;
+const group = animal.characteristics.group;
+const lifeSpan = animal.characteristics.lifespan;
