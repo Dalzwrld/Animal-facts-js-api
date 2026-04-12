05879c4f0e0d7895aab630763d3cbcc79c9729c8
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-12 23:52:56 +0300

    feat: add main js file

--- main.js
+++ main.js
@@ -53,3 +53,66 @@ for (let key in taxonomy) {
 
 const locationTxt = locations.length > 0 ? locations.join(", ") : "Not available";
 
+result.innerHTML = `
+    <div class="card">
+        <img src="${imgUrl}" alt="${name}">
+
+        <div class="card-info">
+            <h2>${name}</h2>
+
+            <div class="taxonomy">
+                <h3>Taxonomy</h3>
+
+                ${taxonomyItems}
+            </div>
+
+            <div class="location">
+                <h3>Locations</h3>
+
+                ${locationTxt}
+            </div>
+
+            <div class="characteristics">
+                <h3>Characteristics</h3>
+
+                <table>
+                    <tr>
+                        <td>
+                            <p>Habitat</p>
+                        </td>
+                        <td>
+                            <p>${habitat}</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p>Diet</p>
+                        </td>
+                        <td>
+                            <p>${diet}</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p>Group</p>
+                        </td>
+                        <td>
+                            <p>${group}</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p>Lifespan</p>
+                        </td>
+                        <td>
+                            <p>${lifespan}</p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+`;
