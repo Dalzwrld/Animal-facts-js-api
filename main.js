940b72eb49fae01bbc3d7bf484b9a2783407cc55
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-13 23:28:46 +0300

    feat: add main js file

--- main.js
+++ main.js
@@ -53,55 +53,38 @@ function renderAnimal(animal) {
     const genus = animal.characteristics.genus || " ";
     const scientificName = animal.characteristics.scientificName || " ";
 
-    let taxonomyItems = "";
-
-    for (let key in animal.taxonomy) {
-        taxonomyItems += `
-            <table>
-                <tr>
-                    <td>
-                        <p>${label}</p>
-                    </td>
-                    <td>
-                        <p>${taxonomy[key]}</p>
-                    </td>
-                </tr>
-            </table>
-        `;
-    }
-
     const container = document.createElement("section");
 
     container.innerHTML = `
     <div class="card-section">
        <div class="animal-name-section">
-           <div class="animal-name">Cheetah</div>
+           <div class="animal-name">${name}</div>
        </div>
 
        <div class="card-container">
            <div class="characteristics">
                <div class="title">
-                   <div class="characteristics2">Characteristics</div>
+                   <div class="characteristics2">${characteristics}</div>
                </div>
 
                 <div class="habitat">
                     <div class="habitat2">Habitat</div>
-                    <div class="habitat-name-grasslands">habitat-name</div>
+                    <div class="habitat-name">${habitat}</div>
                 </div>
 
                 <div class="diet">
                     <div class="diet2">Diet</div>
-                    <div class="diet-name">Carnivore</div>
+                    <div class="diet-name">${diet}</div>
                 </div>
 
                 <div class="group">
                     <div class="group-name">Group</div>
-                    <div class="group-name">Mammal</div>
+                    <div class="group-name">${group}</div>
                 </div>
 
                 <div class="lifespan">
                     <div class="lifespan2">Lifespan</div>
-                    <div class="lifespantxt">10 - 15 years</div>
+                    <div class="lifespantxt">${lifespan}</div>
                 </div>
             </div>
 
@@ -112,7 +95,7 @@ function renderAnimal(animal) {
 
                 <div class="loc0">
                     <div class="location0">0</div>
-                    <div class="location-name">location-name</div>
+                    <div class="location-name">${locations}</div>
                 </div>
             </div>
 
@@ -123,37 +106,37 @@ function renderAnimal(animal) {
 
                 <div class="kingdom">
                     <div class="kingdom2">Kingdom</div>
-                    <div class="kingdom-name"><i>Animalia</i></div>
+                    <div class="kingdom-name"><i>${kingdom}</i></div>
                 </div>
 
                 <div class="phylum">
                     <div class="phylum2">Phylum</div>
-                    <div class="phylum-name"><i>Chordata</i></div>
+                    <div class="phylum-name"><i>${phylum}</i></div>
                 </div>
 
                 <div class="class">
                     <div class="class2">Class</div>
-                    <div class="class-name"><i>Mammalia</i></div>
+                    <div class="class-name"><i>${className}</i></div>
                 </div>
 
                 <div class="order">
                     <div class="order2">Order</div>
-                    <div class="order-name"><i>Carnivora</i></div>
+                    <div class="order-name"><i>${order}</i></div>
                 </div>
 
                 <div class="family">
                     <div class="family2">Family</div>
-                    <div class="family-name"><i>Felidae</i></div>
+                    <div class="family-name"><i>${family}</i></div>
                 </div>
 
                 <div class="genus">
                     <div class="genus2">Genus</div>
-                    <div class="genus-name"><i>Acinonyx</i></div>
+                    <div class="genus-name"><i>${genus}</i></div>
                 </div>
 
                 <div class="scientific-name">
                     <div class="scientific-name2">Scientific name</div>
-                    <div class="scientific-name3"><i>Acinonyx jupatus</i></div>
+                    <div class="scientific-name3"><i>${scientificName}</i></div>
                 </div>
             </div>
         </div>
