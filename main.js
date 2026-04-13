e538a2697ffd3c4e37129861b05be0e2dfe9942d
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-13 14:49:56 +0300

    feat: add main js file

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-let fauna = JSON.parse(localStorage.getItem("animal")) || [];
-
 const API_key = "O6gDOjX0GLmJOBMyTWDoj5RMeMw3Y0LEovT0oWvF";
 
 const animals = ["lion", "cat", "dog", "tiger", "elephant",];
 
 const btn = document.querySelector(".discover-btn");
-
-const animal = document.querySelector(".animal");
+const animalContainer = document.querySelector(".animal-result");
 
 
 const name = animal.name || " ";
@@ -18,6 +15,35 @@ const habitat = animal.characteristics.habitat || " ";
 const diet = animal.characteristics.diet || " ";
 const group = animal.characteristics.group || " ";
 const lifespan = animal.characteristics.lifespan || " ";
+
+
+async function getAnimal() {
+    const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
+
+    const endpoint = `https://api.api-ninjas.com/v1/animals?name=${randomAnimal}`;
+
+    try {
+        const response = await fetch(endpoint, {
+            method: "GET",
+            headers: {
+                "X-API-KEY": API_key
+            }
+        });
+
+        const data = await response.json();
+
+        setTimeout(() => {
+            renderAnimal(data[0]);
+        }, 800);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+    } catch (error) {
+        console.error("Error fetching your animal:", error);
+    }
+}
+
 
 function renderAnimal() {
     animal.innerHTML = "";
@@ -136,29 +162,6 @@ function renderAnimal() {
     animal.appendChild(card);
 }
 
-async function getAnimal() {
-    const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
-
-    const endpoint = `https://api.api-ninjas.com/v1/animals?name=${randomAnimal}`;
-
-    try {
-        const response = await fetch(endpoint, {
-            method: "GET",
-            headers: {
-                "X-API-KEY": API_key
-            }
-        });
-
-        const data = await response.json();
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`)
-        }
-    } catch (error) {
-        console.error("Error fetching your animal:", error);
-        document.querySelector().textContent = "Unable to retrieve animal right now."
-    }
-}
 
 btn.addEventListener("click", async () => {
     saveToLocalStorage();
